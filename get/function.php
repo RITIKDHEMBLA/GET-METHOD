960ab5faae586ee9d67a2ddac576f4e30d2062e4
RITIKDHEMBLA <103738240+RITIKDHEMBLA@users.noreply.github.com>
2022-06-27 10:55:25 +0530

    Add files via upload

---
+++ get/function.php
@@ -0,0 +1,107 @@
+<?php
+
+
+if (isset($_GET['a'])) {
+ //  echo $_GET['a'];
+
+}
+
+
+if (isset($_GET['b'])) {
+   // echo $_GET['b'];
+}
+
+
+if (isset($_GET['c'])) {
+    // echo $_GET['c'];
+ }
+
+
+
+// if($_GET['aa'] > $_GET['bb']){
+// echo  "$_GET[aa]greater then $_GET[bb]<br>"; 
+// } else{
+// echo"$_GET[aa]less then  $_GET[bb]";
+// }
+
+// echo(max($_GET['aa'],$_GET['bb'],$_GET['cc']));
+// echo "is greater";
+
+// echo(min($_GET['aa'],$_GET['bb'],$_GET['cc']));
+// echo "is less";
+
+
+// if (($_GET['aa'] == $_GET['bb'] ) && ($_GET['aa'] == $_GET['cc'] )){
+//     echo 'all are equal';
+// }else{
+//     echo 'some value are not equal';
+// }
+
+// $aa = $_GET['a'];
+
+// if($_GET['a'] > $_GET['b'] && $_GET['a'] > $_GET['c'] ){
+// $aa = $_GET['a'];
+// }
+// if ($_GET['b'] > $_GET['a'] && $_GET['b'] > $_GET['c'])
+// $aa = $_GET['b'];
+// if ($_GET['c'] > $_GET['a'] && $_GET['c'] > $_GET['b'])
+// $aa = $_GET['c'];
+// {
+// echo "$aa IS GREATER";
+// }
+
+// if($_GET['a']>$_GET['b'] && $_GET['a']>$_GET['c']){
+//    echo $_GET['a'];
+// }
+// else{
+//    if($_GET['b']>$_GET['a'] && $_GET['b']>$_GET['c']){
+//        echo $_GET['b'];
+//    }
+//    else
+//    echo $_GET['c'];
+// }
+
+// $min =$_GET['a'];
+// if($_GET['a'] <= $_GET['b'] && $_GET['a'] <= $_GET['c'] )
+// $min = $_GET['a'];
+// if ($_GET['b'] <= $_GET['a'] && $_GET['b'] <= $_GET['c'])
+// $min = $_GET['b'];
+// if ($_GET['c'] <= $_GET['a'] && $_GET['c'] <= $_GET['b'])
+// $min = $_GET['c'];
+// {
+// echo " $min IS Less";
+// }
+
+$a=$_GET["a"];
+
+
+ $b=$_GET["b"];
+
+
+ $c=$_GET["c"];
+
+
+
+
+// if($a>$b && $a>$c){
+//     echo "a";
+// }
+// else{
+//     if($b>$a && $b>$c){
+//         echo "b";
+//     }
+//     else{
+//     echo "c";
+//     }
+// }  
+// if($a<$b && $a<$c){
+//     echo "a";
+// }
+// else{
+//     if($b<$a && $b<$c){
+//         echo "b";
+//     }
+//     else{
+//     echo "c";
+//     }
+// }  
